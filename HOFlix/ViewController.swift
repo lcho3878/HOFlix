6f852faafb73f54e475f868e0b4baba43f81e365
lcho3878 <leech3878@naver.com>
2024-06-04 16:31:46 +0900

    Initial Commit

---
+++ HOFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HOFlix
+//
+//  Created by 이찬호 on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
