378f6db5319f098681bca80f49585666e032690f
lcho3878 <leech3878@naver.com>
2024-06-10 20:19:20 +0900

    CreditVC UI및 오토레이아웃 작성

--- HOFlix/CreditViewController.swift
+++ HOFlix/CreditViewController.swift
@@ -6,12 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class CreditViewController: UIViewController {
 
+    private let backImageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .lightGray
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    private let movieTitleLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Squid Game"
+        lb.textColor = .white
+        lb.textAlignment = .center
+        lb.font = .boldSystemFont(ofSize: 20)
+        return lb
+    }()
+    
+    private let posterImageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .white
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    private let contentTableView: UITableView = {
+        let tb = UITableView()
+        return tb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureHierarchy()
+        configureLayout()
+        configureTableView()
     }
     
     private func configureUI() {
@@ -20,6 +52,71 @@ class CreditViewController: UIViewController {
         navigationItem.title = "출연/제작"
     }
     
+    private func configureHierarchy() {
+        view.addSubview(backImageView)
+        backImageView.addSubview(movieTitleLabel)
+        backImageView.addSubview(posterImageView)
+        view.addSubview(contentTableView)
+    }
+    
+    private func configureLayout() {
+        
+        backImageView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalToSuperview().dividedBy(4)
+        }
+        
+        movieTitleLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(16)
+        }
+        
+        posterImageView.snp.makeConstraints {
+            $0.top.equalTo(movieTitleLabel.snp.bottom).offset(8)
+            $0.bottom.equalToSuperview()
+            $0.leading.equalTo(movieTitleLabel).offset(8)
+            $0.width.equalTo(backImageView).dividedBy(4)
+        }
+        
+        contentTableView.snp.makeConstraints {
+            $0.top.equalTo(backImageView.snp.bottom).offset(8)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+        
+        
+        
+    }
+    
+    private func configureTableView() {
+        contentTableView.dataSource = self
+        contentTableView.delegate = self
+    }
+    
 
 
+}
+
+extension CreditViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 { return "OverView"}
+        else { return "Cast" }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        }
+        else {
+            return 10
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
