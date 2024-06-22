b8cc472e2b63b3bca5dc2464a2889c8f8207cf20
lcho3878 <leech3878@naver.com>
2024-06-22 18:39:01 +0900

    Feat: WeatherViewController 위치 권한 처리

--- HOFlix/WeatherViewController.swift
+++ HOFlix/WeatherViewController.swift
@@ -8,43 +8,132 @@
 import UIKit
 import Alamofire
 import CoreLocation
+import SnapKit
 
 class WeatherViewController: UIViewController {
-
-    let locationManager = CLLocationManager()
+    
+    private let locationManager = CLLocationManager()
+    
+    private let indicator = {
+        let indi = UIActivityIndicatorView()
+        indi.style = .large
+        indi.color = .white
+        return indi
+    }()
+    
+    private let cityLabel = {
+        let lb = UILabel()
+        lb.textColor = .white
+//        lb.text = "도시 이름"
+        return lb
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
-
+        configureUI()
+        configurelocationManager()
+        configureHierarchy()
+        configureLayout()
     }
     
+    private func configureUI() {
+        view.backgroundColor = .systemOrange
+    }
+    
+    private func configureHierarchy() {
+        view.addSubview(cityLabel)
+        view.addSubview(indicator)
+    }
+    
+    private func configureLayout() {
+        cityLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        indicator.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+}
 
-
+extension WeatherViewController {
+    
+    private func checkDeiveLocationAuthorization() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkCurrentLocationAuthorization()
+        }
+        else {
+            print("위치 서비스 꺼져있음")
+        }
+    }
+    
+    private func checkCurrentLocationAuthorization() {
+        let status = locationManager.authorizationStatus
+        
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            print("거부")
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            indicator.startAnimating()
+        default:
+            print(status)
+        }
+    }
+    
 }
 
 extension WeatherViewController: CLLocationManagerDelegate {
+    
+    private func configurelocationManager() {
+        locationManager.delegate = self
+//        locationManager.requestWhenInUseAuthorization()
+//        locationManager.requestLocation()
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let lat = manager.location?.coordinate.latitude
-        let lon = manager.location?.coordinate.longitude
-        let city = "Seoul"
-        let url = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(APIKey.weatherAPIKey)&lang=kr&units=metric"
-        
-        AF.request(url).responseDecodable(of: WeatherResult.self){ response in
-            switch response.result {
-            case .success(let v):
-                print(v)
-            case .failure(let e):
-                print("error: ", e)
-            }
-        }
+        guard let lat = manager.location?.coordinate.latitude,
+              let lon = manager.location?.coordinate.longitude else { return }
+        locationManager.stopUpdatingLocation()
+        callRequest(lat: lat, lon: lon)
         
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
-        print("error")
+        print(#function)
+    }
+    
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+//        print(#function)
+        checkDeiveLocationAuthorization()
+    }
+}
+
+extension WeatherViewController {
+    private func callRequest(lat: CLLocationDegrees, lon: CLLocationDegrees) {
+        let url = "https://api.openweathermap.org/data/2.5/weather"
+        let params: Parameters = [
+            "lat": String(lat),
+            "lon": String(lon),
+            "appid": APIKey.weatherAPIKey,
+            "units": "metric",
+            "lang": "kr"
+        ]
+        AF.request(url, parameters: params)
+            .responseDecodable(of: WeatherResult.self) { response in
+            switch response.result {
+            case .success(let v):
+                print(v.weather)
+                self.cityLabel.text = v.name
+                self.indicator.stopAnimating()
+            case .failure(let e):
+                print(e)
+            }
+        }
     }
 }
 
@@ -52,6 +141,7 @@ struct WeatherResult: Decodable {
     let weather: [Weather]
     let main: [String: Double]
     let wind: [String: Double]
+    let name: String
 }
 
 struct Weather: Decodable {
